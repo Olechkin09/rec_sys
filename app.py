3c42c9bf0cfbc99d579b290d1ef7fb1afea3556a
Olechkin09 <86213997+Olechkin09@users.noreply.github.com>
2022-10-20 15:24:53 +0600

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import numpy as np
 import math
 import streamlit as st
 
+
+
 #Информация о последней статье
 clients_la = pd.read_csv('http://89.108.110.60:3000/public/question/8b6c2d50-32b2-4e9c-a65a-e874d593987b.csv',sep=',')
 counter = len(clients_la.reader.unique().tolist())
@@ -25,9 +27,17 @@ def get_matrix():
   return matrix
 matrix_for_search = get_matrix()
 
+
+
 def rec_system (reader):
+
     interest_list = ((clients_la.loc[clients_la.reader == reader])['article']).tolist()
     last_article = interest_list[0]
+
+    st.write("_**Список прочитанных статей:**_")
+    for j in range( len(interest_list)):
+        st.write( str(j + 1) + '. ' + interest_list[j])
+
     matrix = matrix_for_search[interest_list]
     s = 0 
     l_readers = matrix.index.tolist()
@@ -41,6 +51,11 @@ def rec_system (reader):
             s = 0  
         else: 
             s = 0
+    st.write("_**Список читателей с похожим интересом:**_")
+    for j in range( len(similar_readers[:10])):
+        st.write( str(j + 1) + '. ' + similar_readers[j])
+    st.write("и т.д. Всего " + str(len(similar_readers)) + " читателей.")
+
     rec_list = []
     for reader in similar_readers:
         rec_list.append((clients_la.loc[clients_la.reader ==  reader])['article'].values[0])
@@ -53,28 +68,35 @@ def rec_system (reader):
     rec_list = get_unique_items(rec_list)
     for interest in interest_list:
         rec_list = ([x for x in rec_list  if str(x) != interest])
+    
+    st.write("_**Список статей от читателей с похожими интересами:**_ ")
+    if len(rec_list) > 0:
+        for j in range( len(rec_list[:15])):
+            st.write( str(j + 1) + '. ' + rec_list[j])
+    else:
+     st.write("Данные читатели не могут предложить что-то новое:(")
+
     if len(rec_list) < 15:
+        start_elem =  len(rec_list)
         theme = ((clients_la.loc[clients_la.article == last_article])['theme']).values[0]
+        st.write("_**Список статей по теме:**_  " + theme)
         popular_articles = (popular_articles_all.loc[popular_articles_all.theme == theme])['articles'].tolist()
         rec_list = get_unique_items(rec_list +  popular_articles + popular_articles_list)
-    rec_list = rec_list[0:15]
-    return rec_list
-
-def reader_changed():
-    st.subheader('Сформирован список для ' + st.session_state.reader)
-    rec_list = rec_system (st.session_state.reader)
-    st.write("Выбранный клиент: " + st.session_state.reader)
-    la = (clients_la.loc[clients_la.reader == st.session_state.reader])['article'].values[0]
-    st.write ("Последняя статья: " + la)
-    st.write('_**Что можно порекомендовать:**_')
-    for j in range( len(rec_list)):
-        st.write( str(j + 1) + '. ' + rec_list[j]) 
+        rec_list = rec_list[0:15]
+        for j in range( start_elem, len(rec_list)):
+            st.write( str(j + 1) + '. ' + rec_list[j])
+    rec_list = rec_list[:15]
     df = pd.DataFrame(rec_list)
     df.columns = ['Список рекомендаций']
     df.index = range(1,16)
     df = df.to_csv().encode('utf-8')
-    st.download_button( label="Скачать список", data=df, file_name= 'rec_list_'+ st.session_state.reader +'.csv', mime='text/csv')
+    st.download_button( label="Скачать список", data=df, file_name= 'rec_list_'+ reader +'.csv', mime='text/csv')
     st.write('Список будет скачан в csv формате, открывайте его через приложение "Блокнот"')   
+
+
+def reader_changed():
+    st.subheader('Сформирован список для ' + st.session_state.reader)
+    rec_system (st.session_state.reader)  
 
 reader = st.selectbox("Список рекомендаций появится сверху после выбора читателя", clients_la.reader.unique().tolist(), key="reader", on_change=reader_changed)
 st.markdown("___")
